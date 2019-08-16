355018a9c9708712aeafb4c28a39985437b15464
Himura Kenji <aaron.ariperto@gmail.com>
2019-08-16 10:51:35 -0300

    Se configuro application properties.

---
+++ src/main/java/com/undec/planestudio/PlanEstudioApplication.java
@@ -0,0 +1,13 @@
+package com.undec.planestudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanEstudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanEstudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/undec/planestudio/PlanEstudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.undec.planestudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanEstudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
